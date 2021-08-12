fd4af3b3b3c40d4913210e883603904af842d14d
UsmanA69 <64627750+UsmanA69@users.noreply.github.com>
2021-08-12 16:48:36 +0500

    Add files via upload

---
+++ result app/app.js
@@ -0,0 +1,38 @@
+function result() {
+    var students = {
+        usman: {
+            Maths: 95,
+            Physics: 80,
+            Computer: 50,
+        },
+        ali: {
+            Maths: 56,
+            Physics: 24,
+            Copmuter: 78,
+        },
+        imran: {
+            Maths: 68,
+            Physics: 87,
+            Computer: 98,
+        },
+        aleem: {
+            Maths: 68,
+            Physics: 97,
+            Copmuter: 67,
+        },
+        awais: {
+            Maths: 77,
+            Physics: 96,
+            Computer: 58,
+        }
+    }
+    var studentname = document.getElementById('studentname').value;
+    var input = studentname.toLowerCase();
+    var defination = students[input];
+    var output = document.getElementById('output');
+    if (defination == undefined) {
+        output.innerHTML = `<hr> There is no information about this student! </hr>`;
+    } else {
+        output.innerHTML = `<hr> Maths score is ${defination.Maths}. <hr> Physics score is ${defination.Physics}. <hr> Computer score is ${defination.Computer} <hr>`
+    }
+}
